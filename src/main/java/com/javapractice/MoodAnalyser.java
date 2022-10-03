2dc72f2b9b36fed9e00cd59378389afa4c6b2c84
ilis137 <iliyasshahapure1@gmail.com>
2022-10-03 11:27:24 +0530

    add:display welcome message

---
+++ src/main/java/com/javapractice/MoodAnalyser.java
@@ -0,0 +1,14 @@
+package com.javapractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class MoodAnalyser 
+{
+    private static final Logger log = LogManager.getLogger(MoodAnalyser.class);
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Mood Analyser Program!" );
+    }
+}
