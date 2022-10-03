2772f18d8b9c17fe320ba4fb298a98d77526d930
ilis137 <iliyasshahapure1@gmail.com>
2022-10-03 12:16:41 +0530

    add:handle null exception

--- src/main/java/com/javapractice/MoodAnalyser.java
+++ src/main/java/com/javapractice/MoodAnalyser.java
@@ -3,29 +3,32 @@ package com.javapractice;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-public class MoodAnalyser 
-{
+public class MoodAnalyser {
     private static final Logger log = LogManager.getLogger(MoodAnalyser.class);
     String message;
+
     public MoodAnalyser(String message) {
         this.message = message;
     }
-    
+
     public MoodAnalyser() {
     }
 
     public String analyseMood() {
-    log.info("Welcome to Mood Analyser Program!");
-	if (message.contains("sad"))
-            return "SAD";
-        return "HAPPY";
+        log.info("Welcome to Mood Analyser Program!");
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
     }
-    public static void main( String[] args )
-    {   
-        String message="I am in sad mood";
-        MoodAnalyser analyser=new MoodAnalyser(message);
-        String mood=analyser.analyseMood();
-        log.info("Mood is "+mood);
+
+    public static void main(String[] args) {
+        String message = "I am in sad mood";
+        MoodAnalyser analyser = new MoodAnalyser(message);
+        String mood = analyser.analyseMood();
+        log.info("Mood is " + mood);
     }
 }
